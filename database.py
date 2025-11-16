738270aaada0866b0021682b54b9133f90df9bc2
Pratik Rane <pratikrane0412@gmail.com>
2025-11-17 00:13:26 +0530

    first commit

---
+++ database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Engine
+from sqlalchemy.orm import sessionmaker
+
+db_url = "postgresql://postgres:admin@localhost:5432/pratik"
+engine = create_engine(db_url)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from models import Product
+from database import session, engine
+import database_models
+
+app = FastAPI()
+
+database_models.Base.metadata.create_all(bind=engine)
+
+@app.get("/")
+def greet():
+    return "Welcome to Pat Trac"
+
+products = [
+    Product(id=1, name="Phone", description="A smart phone", price=699.99, quantity=50),
+    Product(id=2, name="Laptop", description="A powerfull laptop", price=999.99, quantity=30),
+    Product(id=3, name="Pen", description="A blue ink pen", price=1.99, quantity=100),
+    Product(id=4, name="Table", description="A wooden table", price=199.99, quantity=20),
+]
+
+
+def init_db():
+    db = session()
+    
+    for product in products:
+        db.add(database_models.Product(**product.model_dump()))
+        
+    db.commit()
+init_db()
+
+@app.get("/products")
+def get_all_products():
+    # db = session()
+    # db.query
+    return products
+
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if product.id == id:
+            return product
+    return "Product Not Found"
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return product
+
+@app.put("/product/")
+def update_product(id: int, product: Product):
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
+            return "Product Added Successfully"       
+    return "Product Not Found"
+
+@app.delete("/product")
+def delete_product(id: int):
+    for i in range(len(products)):
+        if products[i].id == id:
+            del products[i]
+            return "Product Deleted Successfully"
+    return "Product Not Found"
+    
